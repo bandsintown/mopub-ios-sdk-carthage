2c108513f00766c096666dbfef661dc11d2f84a4
Ivan Konov <ikonov@bandsintown.com>
2017-12-27 11:09:54 -0800

    Initial commit with added framework target and Carthage support

---
+++ AdNetworkSupport/TapJoy/TapjoyRewardedVideoCustomEvent.h
@@ -0,0 +1,13 @@
+
+#if __has_include(<MoPub/MoPub.h>)
+    #import <MoPub/MoPub.h>
+#else
+    #import "MPRewardedVideoCustomEvent.h"
+#endif
+
+/*
+ * Certified with Tapjpy version 11.11.0
+ */
+@interface TapjoyRewardedVideoCustomEvent : MPRewardedVideoCustomEvent
+
+@end
